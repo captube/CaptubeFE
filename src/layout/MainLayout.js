0b2d05dee0b6dd0f8555d5719c9eb46cbde34031
jang <jdj9354@naver.com>
2019-03-08 00:32:25 +0900

    Add status on mainlayout

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -6,28 +6,50 @@ import ToggleButton from "../component/ToggleButton";
 import captureButtonSVG from '../res/capture.svg';
 
 class MainLayout extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            status: "init",
+            url: ""
+        }
+    }
+
     render() {
-        return (
-            <div className="MainLayout">
-                <LogoComponent/>
-                <div className="searchDiv">
-                    <RoundedURLInput/>
-                </div>
-                <div className="captureOption">
-                    <div className="title">Capture option</div>
-                    <div className="optionList">
-                        <div className="optionItem" id="isSubOption">
-                            <div className="optionName">
-                                유튜브 자막 추가
-                            </div>
-                            <div className="toggleButton">
-                                <ToggleButton/>
+        if (this.state.status == "init")
+            return (
+                <div className="MainLayout">
+                    <LogoComponent/>
+                    <div className="searchDiv">
+                        <RoundedURLInput/>
+                    </div>
+                    <div className="captureOption">
+                        <div className="title">Capture option</div>
+                        <div className="optionList">
+                            <div className="optionItem" id="isSubOption">
+                                <div className="optionName">
+                                    유튜브 자막 추가
+                                </div>
+                                <div className="toggleButton">
+                                    <ToggleButton/>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        else if (this.state.status == "loading")
+            return (
+                <div className="MainLayout">
+                </div>
+            );
+        else
+            return (
+                <div className="MainLayout">
+                </div>
+            );
+
     }
 }
 
